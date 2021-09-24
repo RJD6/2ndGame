51f1ae30f0e407428c65926cf36595ffeb9aac3d
atenorio3 <68285822+atenorio3@users.noreply.github.com>
2021-09-24 13:52:15 -0500

    Actors Redux

--- 2ndGame.c
+++ 2ndGame.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -17,6 +18,16 @@
 // VRAM update buffer
 #include "vrambuf.h"
 //#link "vrambuf.c"
+
+struct Actor{
+  char label[30]; // What kind of actor is it?
+  byte x; // Current x-location
+  byte y; // Current y-location
+  sbyte dx; // Delta-x
+  sbyte dy; // Delta-y
+  bool is_alive; // Is the actor supposed to be 'alive' right now?
+  byte lives; // How many lives does it have?
+};
 
 // Wait frame to prevent text break or flickering
 // A wait frame does 5 frames per call so...
@@ -65,136 +76,121 @@ const unsigned char name[]={\
         128};
 
 // Make metasprite instances
-DEF_METASPRITE_2x2(majorEnemy, 0xe0, 0); //$07; actors[0]
-DEF_METASPRITE_2x2(minorEnemy, 0xdc, 0); //$06; actors[1]
-DEF_METASPRITE_2x2(player, 0xd8, 0); // $05; actors[2]
+DEF_METASPRITE_2x2(majorEnemy, 0xe0, 0); //$07
+DEF_METASPRITE_2x2(minorEnemy, 0xdc, 0); //$06
+DEF_METASPRITE_2x2(Player, 0xd8, 0); // $05; Capitalized to prevent confusion with player struct
 DEF_METASPRITE_2x2(bullet, 0xe4, 0); // $08
 
-#define NUM_ACTOR_TYPES 4
-// Actors' x/y positions
-byte actor_x[NUM_ACTOR_TYPES];
-byte actor_y[NUM_ACTOR_TYPES];
-// Actors' delta x and delta y
-sbyte actor_dx[NUM_ACTOR_TYPES];
-sbyte actor_dy[NUM_ACTOR_TYPES];
-
-// Place actors on screen
-void setupActors(){
-  // Major Enemy
-  actor_x[0] = 80;
-  actor_y[0] = 10;
-  actor_dx[0] = 1;
-  actor_dy[0] = 0;
-  
-  // Minor Enemy
-  actor_x[1] = 80;
-  actor_y[1] = 30;
-  actor_dx[1] = 1;
-  actor_dy[1] = 0;
-  
-  // Player
-  actor_x[2] = 120;
-  actor_y[2] = 190;
-  actor_dx[2] = 0;
-  actor_dy[2] = 0;
-  
-  // Bullets
-  actor_x[3] = 0;
-  actor_y[3] = 0;
-  actor_dx[3] = 0;
-  actor_dy[3] = -4;
-};
 void main(void)
 {
-  bool bullet_exists = false;
   char i; // Actor Index
   char oam_id; // Sprite ID
+  bool bullet_exists = false; // Is the player's shot currently traveling?
+  struct Actor major_enemies[4];
+  struct Actor minor_enemies[4];
+  struct Actor player;
+  struct Actor bullet_player;
+
+  
+  // Establish Major Enemies
+  for(i=0; i<4; i++){
+    strcpy(major_enemies[i].label, "Major");
+    major_enemies[i].x = 80 + (20 * i);
+    major_enemies[i].y = 10;
+    major_enemies[i].dx = 1;
+    major_enemies[i].dy = 0;
+    major_enemies[i].is_alive = true;
+    major_enemies[i].lives = 1;
+  }
+  
+  // Establish Minor Enemies
+  for(i=0; i<4; i++){
+    strcpy(minor_enemies[i].label, "Minor");
+    minor_enemies[i].x = 80 + (20 * i);
+    minor_enemies[i].y = 30;
+    minor_enemies[i].dx = 1;
+    minor_enemies[i].dy = 0;
+    minor_enemies[i].is_alive = true;
+    minor_enemies[i].lives = 1;
+  }
+  
+  // Establish Player
+  strcpy(player.label, "Player");
+  player.x = 120;
+  player.y = 190;
+  player.dx = 0;
+  player.dy = 0;
+  player.is_alive = true;
+  player.lives = 3;
+  
+  // Establish Player Bullet
+  strcpy(bullet_player.label, "Bullet-Player");
+  bullet_player.x = 0;
+  bullet_player.y = 0;
+  bullet_player.dx = 0;
+  bullet_player.dy = -4;
   
   setup_graphics(); // Setup graphics
-  setupActors(); // Setup Actors
   
   // infinite loop
   while(1) {
-    // Player Movement
     char pad = pad_poll(0);
-    if (pad & PAD_LEFT && actor_x[2]>10){
-      actor_dx[2]=-2;
+    
+    // Track movements for each actor type
+    // Major and Minor Enemies
+    for(i=0; i<4; i++){
+      major_enemies[i].x += major_enemies[i].dx;
+      major_enemies[i].y += major_enemies[i].dy;
+      minor_enemies[i].x += minor_enemies[i].dx;
+      minor_enemies[i].y += minor_enemies[i].dy;
     }
-    else if (pad & PAD_RIGHT && actor_x[2]<230){
-      actor_dx[2]=2;
+    // Player
+    if (pad & PAD_LEFT && player.x>10){
+      player.dx=-2;
+      
+    }
+    else if (pad & PAD_RIGHT && player.x<230){
+      player.dx=2;
     }
     else{
-      actor_dx[2]=0;
+      player.dx=0;
     }
     // Player Attack
     if (pad & PAD_A && !bullet_exists){
       // Bullet must spawn in front of player
-      actor_x[3] = actor_x[2];
-      actor_y[3] = actor_y[2] - 12;
-      oam_id = oam_meta_spr(actor_x[3], actor_y[3], oam_id, bullet);
+      bullet_player.x = player.x;
+      bullet_player.y = player.y - 12;
+      oam_id = oam_meta_spr(bullet_player.x, bullet_player.y, oam_id, bullet);
       bullet_exists = true;
     }
     if (bullet_exists){
       // Bullet must fly forward
-      actor_x[3] += actor_dx[3];
-      actor_y[3] += actor_dy[3];
-      oam_id = oam_meta_spr(actor_x[3], actor_y[3], oam_id, bullet);
-      if (actor_y[3] < 1 || actor_y[3] > 190){
+      bullet_player.x += bullet_player.dx;
+      bullet_player.y += bullet_player.dy;
+      oam_id = oam_meta_spr(bullet_player.x, bullet_player.y, oam_id, bullet);
+      if (bullet_player.y < 1 || bullet_player.y > 190){
         bullet_exists = false;
       }
     }
-    // Track movements for each actor type
-    for(i=0; i<3; i++){
-    actor_x[i] += actor_dx[i];
-    actor_y[i] += actor_dy[i];
-    }
+    
     // Track actor positions
-    for(i=0; i<9; i++){
-      if(i==0 || i==1 || i==2 || i==3){
-        oam_id = oam_meta_spr((actor_x[0]) + (20 * i), actor_y[0], oam_id, majorEnemy);
+    // Major and Minor Enemies
+    for(i=0; i<4; i++){
+      if(major_enemies[i].is_alive){
+        oam_id = oam_meta_spr(major_enemies[i].x, major_enemies[i].y, oam_id, majorEnemy);
       }
-      else if(i==4 || i==5 || i==6 || i==7){
-        oam_id = oam_meta_spr(actor_x[1] + (20 * (i-4)), actor_y[1], oam_id, minorEnemy);
+      if(minor_enemies[i].is_alive){
+        oam_id = oam_meta_spr(minor_enemies[i].x, minor_enemies[i].y, oam_id, minorEnemy);
       }
-      else if(i==8){
-        // Player position and movement
-        oam_id = oam_meta_spr(actor_x[2], actor_y[2], oam_id, player);
-      }
+    }
+    // Player
+    if(player.is_alive){
+      player.x += player.dx;
+      player.y += player.dy;
+      oam_id = oam_meta_spr(player.x, player.y, oam_id, Player);
     }
     
     // wait for next frame (sprites)
     ppu_wait_nmi();
   }
-  
-  // loop forever
-  /*while (1) {
-    // start with OAMid/sprite 0
-    oam_id = 0;
-    // set player 0/1 velocity based on controller
-    for (i=0; i<2; i++) {
-      // poll controller i (0-1)
-      pad = pad_poll(i);
-      // move actor[i] left/right
-      if (pad&PAD_LEFT && actor_x[i]>0) actor_dx[i]=-2;
-      else if (pad&PAD_RIGHT && actor_x[i]<240) actor_dx[i]=2;
-      else actor_dx[i]=0;
-      // move actor[i] up/down
-      if (pad&PAD_UP && actor_y[i]>0) actor_dy[i]=-2;
-      else if (pad&PAD_DOWN && actor_y[i]<212) actor_dy[i]=2;
-      else actor_dy[i]=0;
-    }
-    
-    // draw and move all actors
-    for (i=0; i<NUM_ACTORS; i++) {
-      byte runseq = actor_x[i] & 7;
-      if (actor_dx[i] >= 0)
-        runseq += 8;
-      oam_id = oam_meta_spr(actor_x[i], actor_y[i], oam_id, playerRunSeq[runseq]);
-      actor_x[i] += actor_dx[i];
-      actor_y[i] += actor_dy[i];
-    }
-    // hide rest of sprites
-    // if we haven't wrapped oam_id around to 0
-    if (oam_id!=0) oam_hide_rest(oam_id);
-    */
 }
